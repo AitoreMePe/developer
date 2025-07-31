7866bb00707cffaf021ac29edf70991c29601214
Aitor Mier Pons <aitoremepe@gmail.com>
2025-07-31 21:37:08 +0200

    Use relative imports in package init

--- smol_dev/__init__.py
+++ smol_dev/__init__.py
@@ -1,4 +1,19 @@
-from smol_dev.prompts import *
-from smol_dev.self_heal import run_and_fix
+from .prompts import (
+    file_paths,
+    specify_file_paths,
+    plan,
+    generate_code,
+    generate_code_sync,
+)
+from .self_heal import run_and_fix
+
+__all__ = [
+    "file_paths",
+    "specify_file_paths",
+    "plan",
+    "generate_code",
+    "generate_code_sync",
+    "run_and_fix",
+]
 
 __author__ = "morph"
